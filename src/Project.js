8dfe2c9ba2f5d37af8755099b502507b591200be
Luxe17 <hanson1717@live.com>
2021-04-10 20:14:44 -0500

    Add project functionality

--- src/Project.js
+++ src/Project.js
@@ -1,1 +1,15 @@
 import task from '/src/task.js';
+
+class project {
+    constructor(name){
+        this.name = name;
+    }
+
+    setName(name){
+        this.name = name;
+    }
+
+    getName(){
+        return this.name;
+    }
+}
