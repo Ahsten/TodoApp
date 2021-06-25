2bdc73fb2fffa362d02ca0a5126d24649bf619fe
Luxe17 <hanson1717@live.com>
2021-06-25 13:58:29 -0600

    Add local storage

--- src/storage.js
+++ src/storage.js
@@ -2,5 +2,7 @@ import task from './task';
 import project from './project';
 
 export default class storage{
-
+    static saveTodoList(data){
+        localStorage.setItem('todoList', data);
+    }
 }
